6d6a6ab9a0e205253438b33fc922e2159991c99e
Stefan <1759588+amidabuddha@users.noreply.github.com>
2023-09-08 13:31:18 +0300

    Add prompt with save on exit

--- src/features/exit_chat.rs
+++ src/features/exit_chat.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 
 use crate::{
-    features::save_chat::save_chat,
+    features::save_chat::save_chat_with_prompt,
     models::{api::OpenAIRequest, config::ChatConfig},
 };
 
 pub fn exit_chat(chat_config: &ChatConfig, chat_path: &PathBuf, conversation: &OpenAIRequest) {
     if chat_config.chat.save_chat_on_exit {
-        save_chat(None, chat_path, conversation, true)
+        save_chat_with_prompt(chat_path, conversation)
     };
     println!("Goodbye!");
     std::process::exit(0);
